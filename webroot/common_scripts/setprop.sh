6a6a3f40eb8bd1384c7ba0dd260c352d59686e2b
MONA <121730102+MeowDump@users.noreply.github.com>
2025-09-20 01:12:06 +0530

    ‎

--- webroot/common_scripts/setprop.sh
+++
@@ -1,5 +0,0 @@
-#!/system/bin/sh
-resetprop -p -d persist.sys.pihooks.disable.gms_props
-resetprop -p -d persist.sys.pihooks.disable.gms_key_attestation_block
-echo "Switched back to default settings"
-exit 0
